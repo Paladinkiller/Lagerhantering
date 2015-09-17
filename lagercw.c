7d72aa76955138f7913462a78f095f1a048d8b07
scubathediverz <chubawest@gmail.com>
2015-09-17 12:01:19 +0200

    Update lagercw.c

--- lagercw.c
+++ lagercw.c
@@ -26,11 +26,10 @@ void alternativPrint(void);
 bool taInVal(lager_t *lager);
 void laggTillVara(lager_t *lager);
 void taBortVara(lager_t *lager);
-void redigeraVara(void);
-void listaKorgen(void);
-void angraSenaste(void);
-void lagerSkapning (void);
-void visaVara(void);
+void redigeraVara(lager_t *lager);
+void listaKorgen(lager_t *lager);
+void angraSenaste(lager_t *lager);
+void visaVara(lager_t *lager);
 int main (int argc, char *argv[]){
 vara_t varor[128];
 lager_t lager = {.antal_varor=0, .max_varor=sizeof(varor)/sizeof(vara_t), .varor=varor};
@@ -63,13 +62,13 @@ else if (b == 'T'){
 taBortVara (lager);
 }
 else if (b == 'R'){
-redigeraVara ();
+redigeraVara (lager);
 }
 else if (b == 'G'){
-angraSenaste ();
+angraSenaste (lager);
 }
 else if (b == 'H'){
-listaKorgen ();
+listaKorgen (lager);
 }
 else if (b == 'A'){
 start:
@@ -99,24 +98,27 @@ puts("mata in namnet på varan");
 char buffer[128];
 fgets (buffer, sizeof(buffer), stdin);
 strcpy(vara.namn, buffer);
-printf("%s\n",vara.namn);
+
 puts("mata in beskrivningen på varan");
 char buffer2[256];
 fgets (buffer2, sizeof(buffer2), stdin);
 strcpy(vara.beskrivning, buffer2);
+
 puts("mata in priset på varan");
 int buffer3;
 scanf("%d",&buffer3);
 vara.pris = buffer3;
-printf("%d\n", vara.pris);
+
 puts("mata in lagerplatsens bokstavsindex för varan");
 char buffer4[64];
 fgets (buffer4, sizeof(buffer4), stdin);
 strcpy(vara.lagerplats.bokstav, buffer4);
+
 puts("mata in lagerplatsens nummerindex för varan");
 int buffer5;
 scanf("%d",&buffer5);
 vara.lagerplats.nummer= buffer5;
+
 puts("mata in antalet av varan");
 int buffer6;
 scanf("%d",&buffer6);
@@ -124,13 +126,18 @@ vara.antal = buffer6;
 lager->varor[lager->antal_varor]=vara;
 }
 void taBortVara(lager_t *lager){
+  /*
 printf("%s\n", lager->varor[0].namn);
 printf("%s\n", lager->varor[0].beskrivning);
 printf("%d\n", lager->varor[0].pris);
 printf("%d\n", lager->varor[0].lagerplats.nummer);
 printf("%d\n", lager->varor[0].antal);
-//visaVara() FIXME
+printf("%d\n", lager->antal_varor);
+visaVara() FIXME
+*/
+visaVara(lager);
 
+  /*
 puts ("Ta bort varan? [J]a/[N]ej");
 char b = toupper(getchar());
 while (getchar() !='\n');
@@ -145,10 +152,12 @@ puts("Skriv Ja eller Nej");
 }
 // Måste få programmet att fastna i välja menyn här nu avbryts bara allt
 }
-void redigeraVara(void){
-/*
-visaVara() FIXME
-*/
+  */
+}
+void redigeraVara(lager_t *lager){
+
+  //visaVara() FIXME
+
 char b = toupper(getchar());
 while (getchar() !='\n');
 puts ("[N]amn");
@@ -176,10 +185,10 @@ else if (b == 'A'){
 puts("Fixme Avbryt");
 }
 else {
-redigeraVara ();
+redigeraVara (lager);
 }
 }
-void listaKorgen(void){
+void listaKorgen(lager_t *lager){
 /* for(int i = 0; i < 3; i++) {
 for(int j = 0; j < 3; j++) {
 printf("%d ", lager[i][j]);
@@ -187,21 +196,54 @@ printf("%d ", lager[i][j]);
 printf("\n"); */
 puts ("REEEKT3");
 }
-void angraSenaste(void){
+void angraSenaste(lager_t *lager){
 puts ("REEEKT4");
 }
-void visaVara(void){
-/*
-Visa 20 första varorna i arrayen som en lista 1-20
-*/
-puts("Mata in Numret på varan du vill visa");
-int VisaVaran;
-scanf("%d",&VisaVaran);
-/*
-printf("%c\n",Namn);
-printf("%c\n",Beskrivning);
-printf("%d\n",Pris);
-printf("%c\n",Hyllplats);
-printf("%d\n",Antal);
-*/
+void visaVara(lager_t *lager){
+int lagerindex;
+
+for (lagerindex = 0; lagerindex <= 19; lagerindex += 1);
+  printf("%s\n", lager->varor[lagerindex].namn);
+
+if ((lager->antal_varor) > 20){
+puts("Vill du visa [F]ler varor eller mata in [n]ummer för varan");
+puts("tryck [a] för att avbryta");
+char b = toupper(getchar());
+while (getchar() !='\n');
+  if (b == 'F'){
+  for (lagerindex += 1 ; lagerindex <= 39; lagerindex += 1);
+  printf("%s\n", lager->varor[lagerindex].namn);
+  }
+  else if (b == 'A'){
+  alternativPrint(); //kanske blir fel håll ett öga
+  }
+  else {
+  puts("Mata in nummret för varan");
+  int indexvara;
+  scanf("%d",&indexvara);
+  printf("%s\n", lager->varor[(indexvara-1)].namn);
+  printf("%s\n", lager->varor[(indexvara-1)].beskrivning);
+  printf("%d\n", lager->varor[(indexvara-1)].pris);
+  printf("%s\n", lager->varor[(indexvara-1)].lagerplats.bokstav);
+  printf("%d\n", lager->varor[(indexvara-1)].lagerplats.nummer);
+  printf("%d\n", lager->varor[(indexvara-1)].antal); 
+  }
+ }
+else {
+puts("mata in numret för varan");
+int indexvara;
+scanf("%d",&indexvara);
+printf("%s\n", lager->varor[(indexvara-1)].namn);
+printf("%s\n", lager->varor[(indexvara-1)].beskrivning);
+printf("%d\n", lager->varor[(indexvara-1)].pris);
+printf("%s\n", lager->varor[(indexvara-1)].lagerplats.bokstav);
+printf("%d\n", lager->varor[(indexvara-1)].lagerplats.nummer);
+printf("%d\n", lager->varor[(indexvara-1)].antal);
+ }
 }
+
+
+    
+    
+    
+			      
